e3af5357e1d69513a3a34a1190d740482c2a7a02
MarioSeattle <marioseattle@live.com>
2016-10-01 09:42:36 -0700

    Three Colum layout example

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php ?>
+<footer>
+    <h4>Footer</h4>
+    <p>Footer text</p>
+</footer>
+</div>
+
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<?php ?>
+<html>
+<body>
+<head>
+    <title>Mario's Page</title>
+    <meta charset="UTF-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/styles.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" type="text/css">
+
+</head>
