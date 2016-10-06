a3ccac0c38395f21b4689e48c28538e45dfa36c4
MarioSeattle <marioseattle@live.com>
2016-10-05 18:48:37 -0700

    Header and Footer addition before uploading testing theme on WP

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,16 @@
 <?php ?>
 <html>
+
 <body>
 <head>
     <title>Mario's Page</title>
     <meta charset="UTF-8" />
     <meta name="robots" content="noindex, nofollow" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../css/styles.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet" type="text/css">
-
 </head>
+<header role="banner">
+    <img id="logo-main" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32877/logo-thing.png" width="200" alt="Logo Thing main logo">
+</header>
+<!-- header role="banner" -->
--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <?php ?>
 <?php include("includes/header.php"); ?>
+<style type="text/css">
+</style>
+
 <div id="pagewrap">
 
-    <header>
-        <h1>3 Column Responsive Layout</h1>
-    </header>
+        <section id="top">
+            <h1>Title goes here!</h1>
+        </section>
 
-    <section id="content">
-        <h2>1st Content Area</h2>
-        <p>This page demonstrates a 3 column responsive layout, complete with responsive images and jquery slideshow.</p>
-    </section>
+        <section id="content">
+            <h2>1st Content Area</h2>
+            <p>This page demonstrates a 3 column responsive layout, complete with responsive images and jquery slideshow.</p>
+        </section>
 
-    <section id="middle">
-        <h2>2nd Content Area</h2>
-        <p>At full width all three columns will be displayed side by side. As the page is resized the third column will collapse under the first and second. At the smallest screen size all three columns will be stacked on top of one another.</p>
-        <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-    </section>
+        <section id="middle">
+            <h2>2nd Content Area</h2>
+            <p>At full width all three columns will be displayed side by side. As the page is resized the third column will
+                collapse under the first and second. At the smallest screen size all three columns will be stacked on top of
+                one another.</p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+                vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
+                praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        </section>
 
-    <aside id="sidebar">
-        <h2>3rd Content Area</h2>
-        <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-        <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
-    </aside>
+        <aside id="sidebar">
+            <h2>3rd Content Area</h2>
+            <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat.</p>
+            <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+        </aside>
+
+        <section id="fourth">
+            <h4>Content Title </h4>
+            <p>Lorem ipsum dolor sit amet, semper integer mattis feugiat. Ad orci etiam habitasse etiam. Nam mauris sodales
+                vestibulum tellus, et orci eros tristique, ut lectus rhoncus bibendum libero quam, vitae hac sed, at in. Wisi
+                quisque nec dui, at ut nostra, non dolor, nam id, condimentum consectetuer nulla. Purus orci, libero at libero
+                eu nulla semper, dolor elit faucibus penatibus erat eget sociosqu, faucibus euismod, ipsum ipsum quam porta
+                suspendisse. Convallis arcu condimentum malesuada metus. Curae laborum erat, pellentesque phasellus magna vel.
+                Praesent a, consectetuer placeat ante etiam arcu. Vel pellentesque quam tempor nostra fringilla nam, enim non
+                maecenas risus curabitur leo, vero sapien risus dictum tincidunt a, molestie nec, feugiat praesent molestie.</p>
+        </section>
+
+    </div>
 
     <?php include("includes/footer.php"); ?>
